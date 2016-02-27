6d4695bb6ab62a066a2e5e72d5d263015aca3507
and3rson <anderson.dunai@gmail.com>
2016-02-27 23:05:12 +0200

    Settings.

--- dunai/settings/prod.py
+++ dunai/settings/prod.py
@@ -3,3 +3,14 @@ from .common import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dunai',
+        'HOST': 'localhost',
+        'PORT': 5433,
+        'USER': 'anderson',
+        'PASS': '',
+    }
+}
