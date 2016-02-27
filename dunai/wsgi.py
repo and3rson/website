04485d80ec248222208b8cbf58904faf4c1c86ad
and3rson <anderson.dunai@gmail.com>
2016-02-27 23:39:26 +0200

    Fixes

--- dunai/wsgi.py
+++ dunai/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+from gevent import monkey
+
+monkey.patch_all()
+
+from psycogreen.gevent import patch_psycopg
+
+patch_psycopg()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dunai.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dunai.settings.prod")
 
 application = get_wsgi_application()
