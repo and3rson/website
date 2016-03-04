3e4754ee846b19e54e1e2a45a1cb9fc039e430c1
and3rson <anderson.dunai@gmail.com>
2016-03-04 15:35:11 +0200

    Fix.

--- dunai/chat/urls.py
+++ dunai/chat/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url('^$', views.view, name='view'),
-    url('^set_name$', views.set_name, name='set-name'),
-    url('^poll$', views.poll, name='poll'),
-    url('^send$', views.send, name='send')
+    # url('^$', views.view, name='view'),
+    # url('^set_name$', views.set_name, name='set-name'),
+    # url('^poll$', views.poll, name='poll'),
+    # url('^send$', views.send, name='send')
 ]
