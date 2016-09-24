a7f6408d8fb4a3689f6297bb243bebdbf7c69648
and3rson <anderson.dunai@gmail.com>
2016-09-24 03:23:49 +0300

    Fixes to URL.

--- dunai/posts/urls.py
+++ dunai/posts/urls.py
@@ -3,5 +3,5 @@ from dunai.posts import views
 
 urlpatterns = [
     url('^$', views.view_posts, name='list'),
-    url('^(?P<post_id>\d+)\-(?P<post_slug>[a-zA-Z0-9_-]+)$', views.view_post, name='view')
+    url('^(?P<post_slug>[a-zA-Z0-9_-]+)$', views.view_post, name='view')
 ]
